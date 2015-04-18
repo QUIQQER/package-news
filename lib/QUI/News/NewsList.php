814baea28a07e2826f4ad114cf20521827af84e4
Henning Leutz <leutz@pcsg.de>
2015-04-18 17:46:43 +0200

    psr-2

--- lib/QUI/News/NewsList.php
+++ lib/QUI/News/NewsList.php
@@ -6,6 +6,8 @@
 
 namespace QUI\News;
 
+use QUI;
+
 /**
  * News list helper class
  *
@@ -17,21 +19,21 @@ class NewsList
     /**
      * event on child create
      *
-     * @param Integer $newId
+     * @param Integer                 $newId
      * @param \QUI\Projects\Site\Edit $Parent
      */
     static function onChildCreate($newId, $Parent)
     {
-        if ( $Parent->getAttribute( 'type' ) !== 'quiqqer/news:types/news-list' ) {
+        if ($Parent->getAttribute('type') !== 'quiqqer/news:types/news-list') {
             return;
         }
 
         $Project = $Parent->getProject();
-        $Site    = new \QUI\Projects\Site\Edit( $Project, $newId );
+        $Site = new QUI\Projects\Site\Edit($Project, $newId);
 
-        $Site->setAttribute( 'nav_hide', 1 );
-        $Site->setAttribute( 'release_from', date('Y-m-d H:i:s') );
-        $Site->setAttribute( 'type', 'quiqqer/news:types/news-entry' );
+        $Site->setAttribute('nav_hide', 1);
+        $Site->setAttribute('release_from', date('Y-m-d H:i:s'));
+        $Site->setAttribute('type', 'quiqqer/news:types/news-entry');
         $Site->save();
     }
 }
