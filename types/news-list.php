129cd3d969ccc812f1b18573b1236761baa48aa1
Michael <michael.danielczok@gmail.com>
2017-01-04 10:26:14 +0100

    feat: Childrenlist --> mehrere Layouts integriert fix: showDate und showTime als Attribut fix: Übersetzung für Layouts kommt von Seitentypen

--- types/news-list.php
+++ types/news-list.php
@@ -5,18 +5,19 @@
  */
 
 $ChildrenList = new QUI\Controls\ChildrenList(array(
-    'showTitle'      => false,
-    'showContent'    => false,
-    'showTime'       => $Site->getAttribute('quiqqer.settings.news.showTime'),
-    'showCreator'    => $Site->getAttribute('quiqqer.settings.news.showCreator'),
-    'Site'           => $Site,
-    'where'          => array(
+    'showTitle'       => false,
+    'showContent'     => false,
+    'showTime'        => $Site->getAttribute('quiqqer.settings.news.showTime'), // sich endgültig entscheiden:
+    'showDate'        => $Site->getAttribute('quiqqer.settings.news.showTime'), // showDate vs showTime
+    'showCreator'     => $Site->getAttribute('quiqqer.settings.news.showCreator'),
+    'Site'            => $Site,
+    'where'           => array(
         'type' => 'quiqqer/news:types/news-entry'
     ),
-    'limit'          => $Site->getAttribute('quiqqer.settings.news.max'),
-    'itemtype'       => "http://schema.org/ItemList",
-    'child-itemtype' => "http://schema.org/NewsArticle",
-    'display'        => $Site->getAttribute('quiqqer.settings.news.template'),
+    'limit'           => $Site->getAttribute('quiqqer.settings.news.max'),
+    'itemtype'        => "http://schema.org/ItemList",
+    'child-itemtype'  => "http://schema.org/NewsArticle",
+    'display'         => $Site->getAttribute('quiqqer.settings.news.template'),
     'parentInputList' => false
 ));
 
