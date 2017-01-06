0a12261e7486cd08bc3171cfb25ab11a2aeefcd4
Michael <michael.danielczok@gmail.com>
2017-01-06 16:47:12 +0100

    refactor: locale von Sitentypes refactor: Standardwerte für Settings refactor: 2 Templates umbenannt feat: show time (Uhrzeit)

--- types/news-list.php
+++ types/news-list.php
@@ -7,8 +7,8 @@
 $ChildrenList = new QUI\Controls\ChildrenList(array(
     'showTitle'       => false,
     'showContent'     => false,
-    'showTime'        => $Site->getAttribute('quiqqer.settings.news.showTime'), // sich endgültig entscheiden:
-    'showDate'        => $Site->getAttribute('quiqqer.settings.news.showTime'), // showDate vs showTime
+    'showTime'        => $Site->getAttribute('quiqqer.settings.news.showTime'),
+    'showDate'        => $Site->getAttribute('quiqqer.settings.news.showDate'),
     'showCreator'     => $Site->getAttribute('quiqqer.settings.news.showCreator'),
     'Site'            => $Site,
     'where'           => array(
