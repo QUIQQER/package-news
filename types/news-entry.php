bb2de0775a69eb95396a68c3812d096564c5907e
michael.daniel <michael.danielczok@gmail.com>
2017-08-03 14:21:43 +0200

    feat: Die Möglichkeit Autor und Datum ein- bzw. ausschalten in den Einträgen

--- types/news-entry.php
+++ types/news-entry.php
@@ -1,1 +1,25 @@
 <?php
+
+// default
+$enableDateAndCreator = true;
+$showCreator          = true;
+$showDate             = true;
+
+switch ($Site->getAttribute('quiqqer.settings.news.entry.dateAndCreator')) {
+    case 'showCreator':
+        $showDate = false; // hide date
+        break;
+    case 'showDate':
+        $showCreator = false; // hide author
+        break;
+    case 'hide': {
+        $enableDateAndCreator = false; // disable date and author
+        break;
+    }
+}
+
+$Engine->assign(array(
+    'enableDateAndCreator' => $enableDateAndCreator,
+    'showCreator'          => $showCreator,
+    'showDate'             => $showDate
+));
