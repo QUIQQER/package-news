b2e9ccb3415b3e1c504ca8f3d54c85b09abe4efa
Jan Wennrich <jan.wennrich@pcsg.de>
2024-07-15 14:46:38 +0200

    refactor: remove obsolete error handling
    Variables are no longer empty in QUIQQER 2

--- types/news-list.php
+++ types/news-list.php
@@ -52,17 +52,7 @@ $ChildrenList->addEvent('onMetaList', function (
 
     try {
         // author
-        $UserManager = QUI::getUsers();
-
-        if ($UserManager === null) {
-            throw new \QUI\Exception('Could not load UserManager');
-        }
-
-        $User = $UserManager->get($Site->getAttribute('c_user'));
-
-        if (! $User instanceof \QUI\Interfaces\Users\User) {
-            throw new \QUI\Exception('Could not load user');
-        }
+        $User = QUI::getUsers()->get($Site->getAttribute('c_user'));
 
         $MetaList->add('author', $User->getName());
     } catch (QUI\Exception $Exception) {
